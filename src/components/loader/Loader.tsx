6dfb3211c81ee26c4d722b22239811303e3f0893
Sazidul Alam <sazidulalam20@gmail.com>
2025-03-19 22:32:07 +0600

    loader changed

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,15 +1,9 @@
-import { Flex } from 'antd';
-import { RingLoader } from 'react-spinners';
+import { Flex, Spin } from 'antd';
 
 const Loader = () => {
     return (
         <Flex justify="center" align="center" style={{ height: '85vh' }}>
-            <RingLoader
-                color="#001529"
-                size={100}
-                aria-label="Loading..."
-                data-testid="loader"
-            />
+            <Spin size="large" />
         </Flex>
     );
 };
